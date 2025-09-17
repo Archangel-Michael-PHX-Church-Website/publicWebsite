12b8435e78dca1f137097a4eb437283443bc19fe
sarah <sarah_salib@yahoo.com>
2025-09-17 14:20:58 -0700

    Add Home page, about us and calendar in the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,25 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import React from 'react';
 import Navbar from "./Components/Navbar/Navbar";
+
 import HomePage from "./Pages/Home/Home";
+import About from "./Pages/About/About";
+
 import Footer from "./Components/Footer/Footer";
+import CalendarPage from "./Pages/Calendar/Calendar";
 
 const App: React.FC = () => {
     return (
-        <div>
+        <Router>
             <Navbar />
-            <main>
-                <HomePage/>
-            </main>
-
-                <Footer/>
-
-        </div>
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/calendar" element={<CalendarPage/>}/>
+                {/*<Route path="/contact" element={<Contact />} />*/}
+            </Routes>
+            <Footer/>
+        </Router>
     );
 };
 
