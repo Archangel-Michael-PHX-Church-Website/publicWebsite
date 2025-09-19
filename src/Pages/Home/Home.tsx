9fee792ddd965161a9f584f645554107c7e07874
sarah <sarah_salib@yahoo.com>
2025-09-18 17:26:47 -0700

    fix the home page

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,20 +1,30 @@
 import React from "react";
-import ChurchDroneGif from "../../Components/ChurchDroneGif/ChurchDroneGif";
 import Images from "../../Components/SlideShow/SlideShowImagesData"
 import Slideshow from "../../Components/SlideShow/SlideShow";
 import SubscriptionForm from "../../Components/SubscriptionForm/SubscriptionForm";
+import {EventsCards} from "../../Components/Events/EventsCards";
+import Donation from "../../Components/Donation/Donation";
+// import "./Home.css"
+
 const HomePage : React.FC = () =>{
+
     return(
         <>
-            {/*<ChurchDroneGif/>*/}
-            <div>
-                <Slideshow images={Images} interval={4000}/>
-            </div>
-            <div>
-                <SubscriptionForm/>
-            </div>
+            <div className="home-page">
+                <div>
+                    <Slideshow images={Images} interval={4000}/>
+                </div>
+                <div className="subscription">
+                    <SubscriptionForm />
+                </div>
+                <div className="min-h-screen bg-gray-100">
+                    <EventsCards/>
+                </div>
 
-
+                <div className="donation">
+                    <Donation />
+                </div>
+            </div>
 
         </>
 
