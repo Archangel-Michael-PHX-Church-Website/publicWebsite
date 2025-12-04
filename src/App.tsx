87e616d930e3c20b701f0e1f6fc009eb5ac66db5
sarah <sarah_salib@yahoo.com>
2025-12-04 14:39:09 -0700

    add route pages for the articles and bible study and sunday sermons

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,44 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import React from 'react';
 import Navbar from "./Components/Navbar/Navbar";
-
-import HomePage from "./Pages/Home/Home";
-import About from "./Pages/About/About";
-
 import Footer from "./Components/Footer/Footer";
-import CalendarPage from "./Pages/Calendar/Calendar";
+
+import HomePage from "./Pages/HomePage/HomePage";
+import AboutPage from "./Pages/AboutPage/AboutPage";
+import CalendarPage from "./Pages/CalendarPage/CalendarPage";
+import SermonsPage from "./Pages/SermonsPage/SermonsPage";
+import ArticlesPage from "./Pages/ArticlesPage/ArticlesPage";
+import BibleStudyPage from "./Pages/BibleStudyPage/BibleStudyPage";
+import Intercession from "./Components/Articles/Intercession";
+import Icons from "./Components/Articles/Icons";
+import Eucharist from "./Components/Articles/Eucharist";
+import Priesthood from "./Components/Articles/Priesthood";
+import Sacraments from "./Components/Articles/Sacraments"
+import Traditions from "./Components/Articles/Traditions";
+import ScrollToTop from "./Components/Support/ScrollToTop";
+
+
 
 const App: React.FC = () => {
     return (
         <Router>
+            <ScrollToTop />
             <Navbar />
             <Routes>
                 <Route path="/" element={<HomePage />} />
-                <Route path="/about" element={<About />} />
+                <Route path="/about" element={<AboutPage />} />
                 <Route path="/calendar" element={<CalendarPage/>}/>
-                {/*<Route path="/contact" element={<Contact />} />*/}
+                <Route path="/sermons" element={<SermonsPage/>}/>
+                <Route path="/articles" element={<ArticlesPage/>}/>
+                <Route path="/bible" element={<BibleStudyPage/>}/>
+
+                <Route path="/Intercession" element={<Intercession/>}/>
+                <Route path="/Icons" element={<Icons/>}/>
+                <Route path="/Eucharist" element={<Eucharist/>}/>
+                <Route path="/Priesthood" element={<Priesthood/>}/>
+                <Route path="/Sacraments" element={<Sacraments/>}/>
+                <Route path="/Traditions" element={<Traditions/>}/>
+
             </Routes>
             <Footer/>
         </Router>
